e7800379ad2c8a9774595b340225bf7677318c80
Agney <agney.menon@bigbinary.com>
2022-04-09 02:17:37 +0530

    setup vite to resolve @app/components

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,7 +1,10 @@
+import sum from "@app/components";
+
 function App() {
+  const a = sum(1, 4);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello {a}</h1>
     </div>
   );
 }
